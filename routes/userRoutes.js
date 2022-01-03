52bc467780a6a184e5805afa92c0eeabf8667188
Buff4eyes <claudemokbel@gmail.com>
2022-01-03 10:29:29 -0800

    reg code/ axios script/ redirect reg user

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,6 +8,7 @@ router.post('/users/register', (req,res) => {
   User.register(new User({ username: req.body.username, email: req.body.email}), req.body.password, err => {
     if (err) { console.log(err) }
     res.sendStatus(200)
+    res.redirect('./loginPage.html')
   })
 })
 
